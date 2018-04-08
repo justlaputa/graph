91763e75ba4577655e2ffa2ffb32bf3ef3aee69e
Xiao Han <justlaputa@gmail.com>
2018-04-08 17:54:05 +0900

    add timestamp precision

--- speedtest/speedtest-metrics.sh
+++ speedtest/speedtest-metrics.sh
@@ -13,7 +13,7 @@ timestamp=$(date -d $datetime +"%s")
 
 echo "send metrics to influxdb: ping $ping, download $download, upload $upload, timestamp $timestamp"
 
-curl --max-time 10 -X POST "http://$INFLUXDB_HOST:8086/write?db=mydb" --data-binary "ping,host=laputa-home-nuc value=$ping $timestamp
+curl --max-time 10 -X POST "http://$INFLUXDB_HOST:8086/write?db=mydb&precision=s" --data-binary "ping,host=laputa-home-nuc value=$ping $timestamp
 download,host=laputa-home-nuc value=$download $timestamp
 upload,host=laputa-home-nuc value=$upload $timestamp"
 
